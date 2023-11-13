80179b796c3b4d9e89476de597454c92d426505f
TechAkayy <techakayy@gmail.com>
2023-11-14 02:38:35 +1100

    first commit

---
+++ packages/my-shadcn-vue-layer/nuxt.config.ts
@@ -0,0 +1,24 @@
+// import { fileURLToPath } from 'url'
+// import { dirname, join } from 'path'
+
+// const currentDir = dirname(fileURLToPath(import.meta.url))
+
+export default defineNuxtConfig({
+  // modules: ['@nuxtjs/tailwindcss'], // Already added by nuxt-ui
+  // tailwindcss: {
+  //   cssPath: join(currentDir, './assets/css/tailwind.css'),
+  // },
+  hooks: {
+    'components:dirs': (dirs) => {
+      dirs.unshift({
+        path: '~/components/ui',
+        // this is required else Nuxt will autoImport `.ts` file
+        extensions: ['.vue'],
+        // prefix for your components, eg: SvCalendar
+        prefix: 'Sv',
+        // prevent adding another prefix component by it's path.
+        pathPrefix: false,
+      })
+    },
+  },
+})
